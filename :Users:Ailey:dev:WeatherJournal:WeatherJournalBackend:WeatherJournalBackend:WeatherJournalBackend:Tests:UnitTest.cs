d1fc985ab250990b9db3c8e45658a264e7618ff3
Ailey <rong_25@hotmail.com>
2019-01-25 18:40:38 -0500

    Added separate unit test project with 'invalid setup on non-virtual member' error message

---
+++ :Users:Ailey:dev:WeatherJournal:WeatherJournalBackend:WeatherJournalBackend:WeatherJournalBackend:Tests:UnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace WeatherJournalBackend.Tests {
+
+    //[TestFixture]
+    //public class UnitTest {
+    //    public UnitTest() {
+    //    }
+
+    //    public void Test() {
+    //        Assert.That(true, Is.False);
+    //    }
+    //}
+}
---
+++ WeatherJournalBackend/WeatherJournalBackend/TestLibrary/UnitTest1.cs
@@ -0,0 +1,38 @@
+using System;
+using NUnit.Framework;
+using Moq;
+using WeatherJournalBackend.Entities;
+using WeatherJournalBackend.Services;
+using WeatherJournalBackend.UoW;
+
+namespace TestLibrary {
+    [TestFixture]
+    public class UnitTest1 {
+        //[Test]
+        //public void TestFail() {
+        //    Assert.That(true, Is.False);
+        //}
+
+        [Test]
+        public void GetUserById_ValidId() {
+            User validUser = new User {
+                Id = "2",
+                FirstName = "old first name",
+                LastName = "old new name",
+                Username = "oldUser123"
+            };
+
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            unitOfWorkMock.Setup(x => x.UserRepository.GetUserById(validUser.Id))
+                          .Returns(validUser);
+
+            UserService userService = new UserService(unitOfWorkMock.Object);
+            var actual = userService.GetUser(validUser.Id);
+
+            Assert.IsTrue(actual != null);
+            Assert.IsTrue(actual.FirstName == "old first name");
+            Assert.IsTrue(actual.LastName == "old new name");
+            Assert.IsTrue(actual.Username == "oldUser123");
+        }
+    }
+}
